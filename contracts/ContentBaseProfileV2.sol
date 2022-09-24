06e4ea316e043dc9c970e5e41a742b1841c5b7f9
Autsada <autsada.t@gmail.com>
2022-09-24 20:12:18 +0700

    update profile smart contract

---
+++ contracts/ContentBaseProfileV2.sol
@@ -0,0 +1,266 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol";
+import "hardhat/console.sol";
+
+import {Constants} from "../libraries/Constants.sol";
+
+contract ContentBaseProfileV2 is
+    Initializable,
+    ERC721Upgradeable,
+    AccessControlUpgradeable,
+    UUPSUpgradeable
+{
+    using CountersUpgradeable for CountersUpgradeable.Counter;
+
+    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+    bytes32 public constant UPGRADER_ROLE = keccak256("UPGRADER_ROLE");
+
+    CountersUpgradeable.Counter private _profileIdCounter;
+    // array of profile ids by address
+    mapping(address => uint256[]) private _profileIdsByAddress;
+    // profile id by handle hash
+    mapping(bytes32 => uint256) private _profileIdByHandleHash;
+    // profile struct by profile id
+    mapping(uint256 => Profile) private _profileById;
+
+    /**
+     * @dev  profileId - a token id
+     * @dev  owner - an address who owns the token
+     * @dev uid - a database user id or wallet address
+     * @dev handle - a user given name which must be unique
+     * @dev imageURI1 - an image uri save on ipfs
+     * @dev imageURI2 - an image uri save on cloud storage
+     * @dev isDefault - boolean if the owner want a profile be their default profile
+     */
+    struct Profile {
+        uint256 profileId;
+        address owner;
+        string uid;
+        string handle;
+        string imageURI1;
+        string imageURI2;
+        bool isDefault;
+    }
+
+    struct CreateProfileArgs {
+        string uid;
+        string handle;
+        string imageURI1;
+        string imageURI2;
+        bool isDefault;
+    }
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    event ProfileCreated(uint256 profileId, address owner);
+
+    function initialize() public initializer {
+        __ERC721_init("Content Base Profile", "CTB");
+        __AccessControl_init();
+        __UUPSUpgradeable_init();
+        _setRoleAdmin(DEFAULT_ADMIN_ROLE, DEFAULT_ADMIN_ROLE);
+        _setRoleAdmin(ADMIN_ROLE, ADMIN_ROLE);
+        _setRoleAdmin(UPGRADER_ROLE, ADMIN_ROLE);
+
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(ADMIN_ROLE, msg.sender);
+        _grantRole(UPGRADER_ROLE, msg.sender);
+    }
+
+    /**
+     * @dev modifier to check if handle has correct length
+     */
+    modifier _onlyValidHandleLen(string memory handle) {
+        bytes memory bytesHandle = bytes(handle);
+        if (
+            bytesHandle.length >= Constants.MIN_HANDLE_LENGTH &&
+            bytesHandle.length <= Constants.MAX_HANDLE_LENGTH
+        ) {
+            _;
+        } else {
+            revert(
+                "Handle must be at least 3 and not greater than 25 characters."
+            );
+        }
+    }
+
+    /**
+     * @dev modifier to validate image uri
+     */
+    modifier _onlyValidImageURI(string memory imageURI) {
+        bytes memory bytesURI = bytes(imageURI);
+        if (Constants.MAX_PROFILE_IMAGE_URI_LENGTH >= bytesURI.length) {
+            _;
+        } else {
+            revert("Image uri is too long.");
+        }
+    }
+
+    /**
+     * @dev an external function that users can call to create profiles
+     *
+     * @param createProfileArgs.uid - the uid of the user. If using the built-in wallet it's a uid from external database, if using user own wallet it's an address.
+     * @param createProfileArgs.handle - a user given name of the profile
+     * @param createProfileArgs.imageURI1 - an image uri save on ipfs
+     * @param createProfileArgs.imageURI2 - an image uri save on cloud storage
+     * @param createProfileArgs.isDefault - if the profile is a default profile of the user.
+     */
+    function createProfile(CreateProfileArgs memory createProfileArgs)
+        external
+        _onlyValidHandleLen(createProfileArgs.handle)
+        _onlyValidImageURI(createProfileArgs.imageURI1)
+        _onlyValidImageURI(createProfileArgs.imageURI2)
+        returns (uint256)
+    {
+        return
+            _createProfile({
+                owner: msg.sender,
+                uid: createProfileArgs.uid,
+                handle: createProfileArgs.handle,
+                imageURI1: createProfileArgs.imageURI1,
+                imageURI2: createProfileArgs.imageURI2,
+                isDefault: createProfileArgs.isDefault
+            });
+    }
+
+    /**
+     * @dev a function that actually contains logic to create a profile.
+     *
+     */
+    function _createProfile(
+        address owner,
+        string memory uid,
+        string memory handle,
+        string memory imageURI1,
+        string memory imageURI2,
+        bool isDefault
+    ) internal returns (uint256) {
+        // Check if handle is already taken
+        require(_handleUnique(handle), "Handle already taken.");
+
+        _profileIdCounter.increment();
+        uint256 newProfileId = _profileIdCounter.current();
+
+        // Mint new token
+        _safeMint(owner, newProfileId);
+
+        // Push the profile id to user's profile ids array
+        _profileIdsByAddress[owner].push(newProfileId);
+
+        // Link profile id to handle hash
+        _profileIdByHandleHash[_hashHandle(handle)] = newProfileId;
+
+        // Create a profile struct and assign it to the newProfileId in the mapping.
+        _profileById[newProfileId] = Profile({
+            profileId: newProfileId,
+            owner: owner,
+            uid: uid,
+            handle: handle,
+            imageURI1: imageURI1,
+            imageURI2: imageURI2,
+            isDefault: isDefault
+        });
+
+        // Emit an event
+        emit ProfileCreated(newProfileId, owner);
+
+        return newProfileId;
+    }
+
+    function totalProfiles() public view returns (uint256) {
+        return _profileIdCounter.current();
+    }
+
+    // Function to fetch user's profiles
+    function fetchMyProfiles(address owner)
+        public
+        view
+        returns (Profile[] memory)
+    {
+        // Get the profile ids array of the owner
+        uint256[] memory profileIds = _profileIdsByAddress[owner];
+
+        if (profileIds.length == 0) revert("Not found");
+
+        // Create a profiles array in memory with the fix size of ids array length
+        Profile[] memory profiles = new Profile[](profileIds.length);
+
+        // Loop through the ids array and get the profile for each id
+        for (uint256 i = 0; i < profileIds.length; i++) {
+            profiles[i] = _profileById[profileIds[i]];
+        }
+
+        return profiles;
+    }
+
+    /**
+     * @dev A function to validate handle - validate len and uniqueness
+     */
+    function validateHandle(string calldata handle)
+        external
+        view
+        _onlyValidHandleLen(handle)
+        returns (bool)
+    {
+        return _handleUnique(handle);
+    }
+
+    // A function to check if handle is unique
+    function _handleUnique(string memory handle) internal view returns (bool) {
+        return _profileIdByHandleHash[_hashHandle(handle)] == 0;
+    }
+
+    // A function to hash a handle
+    function _hashHandle(string memory handle) internal pure returns (bytes32) {
+        return keccak256(bytes(handle));
+    }
+
+    function _authorizeUpgrade(address newImplementation)
+        internal
+        override
+        onlyRole(UPGRADER_ROLE)
+    {
+        _grantRole(UPGRADER_ROLE, newImplementation);
+    }
+
+    /**
+     * @notice If it's not the first creation of the token sender must be an owner of the token and is equal to "from" and "to", so token is non-transferable
+     *
+     */
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal override(ERC721Upgradeable) {
+        if (from != address(0)) {
+            require(
+                (msg.sender == ownerOf(tokenId)) &&
+                    (msg.sender == from) &&
+                    (msg.sender == to),
+                "Profile is non-transferable"
+            );
+        }
+
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721Upgradeable, AccessControlUpgradeable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
---
+++ scripts/deploy_upgrade_contentbase_profile.ts
@@ -0,0 +1,32 @@
+import { ethers, upgrades } from 'hardhat'
+import path from 'path'
+import fs from 'fs/promises'
+
+import contentBaseProfile from '../abi/ContentBaseProfile.json'
+
+async function main() {
+  const ContentBaseV2 = await ethers.getContractFactory('ContentBaseProfileV2')
+  const contentBaseV2 = await upgrades.upgradeProxy(
+    contentBaseProfile.address,
+    ContentBaseV2
+  )
+
+  await contentBaseV2.deployed()
+
+  console.log('ContentBase deployed to:', contentBaseV2.address)
+  //Pull the address and ABI out, since that will be key in interacting with the smart contract later
+  const data = {
+    address: contentBaseV2.address,
+    abi: JSON.parse(contentBaseV2.interface.format('json') as string),
+  }
+
+  await fs.writeFile(
+    path.join(__dirname, '..', '/abi/ContentBaseProfileV2.json'),
+    JSON.stringify(data)
+  )
+}
+
+main().catch((error) => {
+  console.error('error: ', error)
+  process.exitCode = 1
+})
