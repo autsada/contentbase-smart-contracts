88bd4b25f260c5eaa36db696f4ec51a7079bea8b
Autsada <autsada.t@gmail.com>
2022-09-26 17:30:44 +0700

    feat:update to profile v3

--- libraries/Constants.sol
+++ libraries/Constants.sol
@@ -3,6 +3,6 @@ pragma solidity ^0.8.9;
 
 library Constants {
     uint8 internal constant MIN_HANDLE_LENGTH = 3;
-    uint8 internal constant MAX_HANDLE_LENGTH = 50;
-    uint16 internal constant MAX_PROFILE_IMAGE_URI_LENGTH = 3000;
+    uint8 internal constant MAX_HANDLE_LENGTH = 40;
+    uint16 internal constant MAX_PROFILE_IMAGE_URI_LENGTH = 4000;
 }
