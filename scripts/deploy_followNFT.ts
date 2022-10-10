bac3c0605183272ad2615d12131058bc0b214191
Autsada <autsada.t@gmail.com>
2022-10-10 19:51:45 +0700

    feat:add follow deploy script

---
+++ scripts/deploy_followNFT.ts
@@ -0,0 +1,27 @@
+import { ethers, upgrades } from 'hardhat'
+import path from 'path'
+import fs from 'fs/promises'
+
+async function main() {
+  const FollowNFTContract = await ethers.getContractFactory('FollowNFT')
+  const followNFTContract = await upgrades.deployProxy(FollowNFTContract)
+
+  await followNFTContract.deployed()
+
+  console.log('FollowNFTContract deployed to:', followNFTContract.address)
+  //Pull the address and ABI out, since that will be key in interacting with the smart contract later
+  const data = {
+    address: followNFTContract.address,
+    abi: JSON.parse(followNFTContract.interface.format('json') as string),
+  }
+
+  await fs.writeFile(
+    path.join(__dirname, '..', '/abi/FollowNFTContract.json'),
+    JSON.stringify(data)
+  )
+}
+
+main().catch((error) => {
+  console.error('error: ', error)
+  process.exitCode = 1
+})
